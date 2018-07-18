eda96db5a1fc1e14a28361288c692593fceb706c
tkhr.sait <tkhr.sait@gmail.com>
2018-07-18 09:17:27 +0900

    feat(flowgen): merge

--- README.md
+++ README.md
@@ -9,17 +9,21 @@ flow generator for Node-RED.
 ## Requirement
 
 node-red  
+argv-parser ... helpが出しやすいものに変更（あとで）  
 swagger-parser  
-fs  
 
 ## Usage
 
 ```
 npm i
-node flowgen.js input output
+node flowgen.js -i input -o output
 
 ex)
-node flowgen.js https://petstore.swagger.io/v2/swagger.json /tmp/flows.json
+node flowgen.js -i https://petstore.swagger.io/v2/swagger.json -o /tmp/flows.json
+
+ex) merge
+node flowgen.js -i /path/to/swagger.json -o /tmp/flows.json -m
+
 ```
 
 ## Install
@@ -35,28 +39,31 @@ Apache License 2.0
 
 ## Todo
 
-* [ ] flow merge  
-[ ] 仕様整理  
-[ ] 引数:モード用意。マージ or 上書き  
-[ ] 既存フロー読み込み  
-[ ] 既存フローvalidate  
-[ ] 既存フローとswagger付き合わせ  
-[ ] 振り分け(コメントで「add/del」)  
+* [x] flow merge  
+[x] 仕様整理  
+[x] 引数:モード用意。マージ or 上書き  
+[x] 既存フロー読み込み  
+[x] 既存フローcheck  
+[x] 既存フローとswagger付き合わせ  
+[x] 振り分け(コメントで「add/del」)  
+* [ ] output swagger（node-red-contrib-swaggerString）  
+[ ] 振り分け(コメントで「mod」)
 * [ ] output example
-* [ ] output swagger（node-red-node-swagger）  
 
 ## Feature
 
 ### flow merge
 * 引数 -m/--merge が指定された場合
-* 指定されたflowがnode-redのものかvalidateし…できんの？
-* endpoint(url)  をキーにマージする  
+* 指定されたflowがnode-redのものかvalidate
+* endpoint(url)+method をキーにマージする  
 
 |ケース|既存フロー|生成フロー|動作|
 |-----|--------|--------|----|
-|変更|有り|有り|何もしない|
+|変更|有り|有り|何もしない[0.0.3あたりで比較追加※]|
 |削除|有り|無し|既存フローにコメント(削除)|
 |追加|無し|有り|既存フローにコメント(追加)|
 
-[FIXME]swagger情報をhttp inに持たせる場合、
-差分チェック必要
+* ※比較のためのswagger情報をhttp inに持たせる場合、node-red-node-swaggerのように
+  http inに拡張情報をもたせる必要あり
+
+### check modify
--- flowgen.js
+++ flowgen.js
@@ -1,30 +1,60 @@
 #!/usr/bin/env node
-var RED = require('node-red')
+'use strict'
+
 var fs = require('fs')
+var RED = require('node-red')
 var SwaggerParser = require('swagger-parser')
 
-if (process.argv.length !== 4) {
-  console.error('usage: flowgen.js input output')
-  process.exit(2)
+var arg = parseArguments(process.argv)
+var input = arg.parsed.input
+var output = arg.parsed.output
+var flowName = arg.parsed.flowName
+var merge = arg.parsed.merge
+
+// defult x,y
+var x = 200
+var y = 50
+
+// merge output
+var json = null
+var flowId = null
+if (merge === true) {
+  try {
+    json = JSON.parse(fs.readFileSync(output))
+  } catch (err) {
+    console.error(err)
+    process.exit(2)
+  }
+  // search flow & getId(validate...)
+  for (var idx in json) {
+    if (json[idx].type === 'tab' && json[idx].label === flowName) {
+      flowId = json[idx].id
+    }
+  }
+}
+if (flowId === null) {
+  // create new flow
+  var flow = createFlowNode(flowName)
+  json = [ flow ]
+  flowId = flow.id
 }
 
-var input = process.argv[2]
-var output = process.argv[3]
-
-// create flow
-var flowId = RED.util.generateId()
-var flowName = 'Swagger API'
-var json = [{
-  'id': flowId,
-  'type': 'tab',
-  'label': flowName,
-  'disabled': false,
-  'info': ''
-}]
+// calc max y position & collect api
+var apis = []
+if (merge === true) {
+  for (var index in json) {
+    if (json[index].z === flowId) {
+      if (json[index].y >= y) {
+        y = json[index].y + 50
+      }
+      if (isHttpInNode(flowId, json[index])) {
+        apis.push(json[index].url + ',' + json[index].method)
+      }
+    }
+  }
+}
 
 // create nodes
-var x = 100
-var y = 50
 var templateString = ''
 
 // read from swagger.json
@@ -34,15 +64,67 @@ SwaggerParser.validate(input)
       .then(function (api) {
         for (var path in api.paths) {
           for (var pathMethod in api.paths[path]) {
-            console.log(api.basePath + path + '[' + pathMethod + ']')
             // TODO example
             templateString = ''
-            createHttpNode(json, flowId, (api.basePath + path).replace(/\{[^}]*\}/g, '*'), pathMethod, templateString, x, y)
-            y += 50
+            var hit = false
+            if (merge === true) {
+              // search url+method in 'http in' nodes
+              for (var idx in json) {
+                if (isHttpInNode(flowId, json[idx]) &&
+                    json[idx].url === convertUrl(api.basePath, path) &&
+                    json[idx].method === pathMethod) {
+                  hit = true
+
+/* FIXME node-red-node-swagger のように 'http in' プロパティ拡張しないと値保持できない
+                  // check modify
+                  if (json[idx].swaggerString !== JSON.stringify(api.paths[path][pathMethod])) {
+                    // add modified comment
+                    console.log('modify. [' + json[idx].method + ']' + json[idx].url)
+                    json.push(createComment(flowId, 'modify. [' + json[idx].method + ']' + json[idx].url, '', 100, json[idx].y - 25))
+                    json[idx].swaggerString = JSON.stringify(api.paths[path][pathMethod])
+                  }
+*/
+
+                  // remove from apis
+                  apis = apis.filter(function (value) {
+                    return value !== (json[idx].url + ',' + json[idx].method)
+                  })
+                  break
+                }
+              }
+            }
+            if (hit === false) {
+              // add new api comment
+              console.log('added. [' + pathMethod + '] ' + convertUrl(api.basePath, path))
+              json.push(createComment(flowId, 'added. [' + pathMethod + '] ' + convertUrl(api.basePath, path), '', 100, y - 25))
+              createHttpNode(json, flowId, convertUrl(api.basePath, path), pathMethod, JSON.stringify(api.paths[path][pathMethod]), templateString, x, y)
+              y += 50
+            }
           }
         }
+        // check deleted api
+        if (merge === true) {
+          for (var index in json) {
+            if (isHttpInNode(flowId, json[index])) {
+              for (var deletedApi in apis) {
+                if (apis[deletedApi] === json[index].url + ',' + json[index].method) {
+                  // add deleted api comment
+                  console.log('deleted. [' + json[index].method + '] ' + json[index].url)
+                  json.push(createComment(flowId, 'deleted. [' + json[index].method + '] ' + json[index].url, '', 100, json[index].y - 25))
+                  break
+                }
+              }
+            }
+          }
+        }
+        // output
         var jsonstr = JSON.stringify(json, null, 4)
-        fs.writeFileSync(output, jsonstr)
+        try {
+          fs.writeFileSync(output, jsonstr)
+        } catch (err) {
+          console.error(err)
+          process.exit(2)
+        }
         console.log('generate flow success.')
         process.exit(0)
       })
@@ -53,7 +135,89 @@ SwaggerParser.validate(input)
   })
 
 // functions -------------------------------------------------------------------
-function createHttpNode (json, flowId, url, method, templateString, x, y) {
+function parseArguments (argv) {
+  var ArgvParser = require('argv-parser')
+  // parse options ...
+  var argrules = {
+    input: {
+      type: String,
+      short: 'i',
+      value: function (value, parsed, tool) {
+        if (value === undefined) {
+          tool.error('required value')
+        }
+        return value
+      }
+    },
+    output: {
+      type: String,
+      short: 'o',
+      value: function (value, parsed, tool) {
+        if (value === undefined) {
+          tool.error('required value')
+        }
+        return value
+      }
+    },
+    flowName: {
+      type: String,
+      short: 'f',
+      value: function (value, parsed, tool) {
+        if (value === undefined) {
+          value = 'Swagger API'
+        }
+        return value
+      }
+    },
+    merge: {
+      type: Boolean,
+      short: 'm',
+      value: false
+    }
+  }
+  var arg = ArgvParser.parse(process.argv, {rules: argrules})
+  if (JSON.stringify(arg.errors) !== '{}') {
+    console.error(arg.errors)
+    process.exit(2)
+  }
+  return arg
+}
+function convertUrl (basePath, path) {
+  return (basePath + path).replace(/\{[^}]*\}/g, '*')
+}
+function isHttpInNode (flowId, node) {
+  if (node.z === flowId &&
+      node.type === 'http in') {
+    return true
+  }
+  return false
+}
+function createFlowNode (flowName) {
+  var flowId = RED.util.generateId()
+  var json = {
+    'id': flowId,
+    'type': 'tab',
+    'label': flowName,
+    'disabled': false,
+    'info': ''
+  }
+  return json
+}
+function createComment (flowId, title, detail, x, y) {
+  var commentId = RED.util.generateId()
+  var json = {
+    'id': commentId,
+    'type': 'comment',
+    'z': flowId,
+    'name': title,
+    'info': detail,
+    'x': x,
+    'y': y,
+    'wires': []
+  }
+  return json
+}
+function createHttpNode (json, flowId, url, method, swaggerString, templateString, x, y) {
   // create http nodes
   var httpInNodeId = RED.util.generateId()
   var templateNodeId = RED.util.generateId()
@@ -66,6 +230,7 @@ function createHttpNode (json, flowId, url, method, templateString, x, y) {
     'url': url,
     'method': method,
     'upload': false,
+//    'swaggerString': swaggerString, // FIXME
     'x': x,
     'y': y,
     'wires': [ [ templateNodeId ] ]
